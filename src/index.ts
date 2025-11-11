b2079a6abf57da616e61de24e387fd2457758c73
neonphog <neonphog@gmail.com>
2025-11-11 15:32:23 -0700

    cleaner start code

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
+// Import the voidmerge-code library to gain access to types.
 import * as VM from "@voidmerge/voidmerge-code"
 
+// The "VoidMergeHandler" is the main entry point to void merge logic.
 VM.defineVoidMergeHandler(async (req) => {
-  const type = req.type;
-
-  if (req instanceof VM.RequestObjCheck) {
-    // All data is valid:
-    return new VM.ResponseObjCheckOk();
-  } else if (req instanceof VM.RequestFn) {
+  // If this is a "Function" type request, send back a "Hello World!" response.
+  if (req instanceof VM.RequestFn) {
     return new VM.ResponseFnOk({
+      // Set the http status code to 200.
       status: 200,
+
+      // Send back "Hello World!" text.
       body: new TextEncoder().encode("Hello World!"),
+
+      // Set the content type to "text/plain".
       headers: {
         "content-type": "text/plain",
       },
     });
   }
 
-  throw new Error(`invalid request type: ${type}`);
+  // Output an error if the request is a type we don't handle.
+  throw new Error(`Unhandled Request ${JSON.stringify(req)}`);
 })
